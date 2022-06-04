7d71fa8dd4d232666f404ae366786bb51f3922c5
ablsoftware <105504209+ablsoftware@users.noreply.github.com>
2022-06-04 12:46:17 +0800

    Add files via upload

---
+++ template/index.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ *
+ * 音乐搜索器 - 模版文件
+ *
+ * @author  MaiCong <i@maicong.me>
+ * @link    https://github.com/maicong/music
+ * @since   1.5.7
+ *
+ */
+
+if (!defined('MC_CORE')) {
+    header("Location: /");
+    exit();
+}
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>菠萝软件|云音乐</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-Control" content="no-transform">
+    <meta http-equiv="Cache-Control" content="no-siteapp">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="author" content="maicong.me">
+    <meta name="keywords" content="音乐搜索,音乐搜索器,音乐试听,音乐在线听,网易云音乐,QQ音乐,酷狗音乐,酷我音乐,虾米音乐,百度音乐,一听音乐,咪咕音乐,荔枝FM,蜻蜓FM,喜马拉雅FM,全民K歌,5sing原创翻唱音乐">
+    <meta name="description" content="菠萝软件多站合一音乐搜索解决方案，可搜索试听网易云音乐、QQ音乐、酷狗音乐、酷我音乐、虾米音乐、百度音乐、一听音乐、咪咕音乐、荔枝FM、蜻蜓FM、喜马拉雅FM、全民K歌、5sing原创翻唱音乐。">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="音乐搜索器">
+    <meta name="application-name" content="音乐搜索器">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="https://z3.ax1x.com/2021/07/07/RHaUc8.jpg">
+    <link rel="apple-touch-icon" href="https://z3.ax1x.com/2021/07/07/RHaUc8.jpg">
+    <link rel="canonical" href="http://music.2333.me/">
+    <link rel="stylesheet" href="//cdn.bootcss.com/amazeui/2.3.0/css/amazeui.min.css">
+    <link rel="stylesheet" href="static/css/style.css?v<?php echo MC_VERSION; ?>">
+</head>
+<body>
+    <!--[if lte IE 9]>
+        <script type="text/javascript">
+            (function(){
+                var t = '你的浏览器也太挫了吧！大佬换一个噻！';
+                document.body.innerHTML = t;
+                document.body.style.fontSize = '66px';
+                document.body.style.textAlign = 'center';
+                document.body.style.background = '#000';
+                document.body.style.color = '#fff';
+                if (prompt('输入代号 666666 销毁此电脑: ', '') === '666666') {
+                    alert('拜拜了您呢~')
+                } else {
+                    alert('总感觉哪里不对');
+                }
+                window.open('', '_self', '');
+                window.close();
+            })();
+        </script>
+    <![endif]-->
+    <section class="am-g about">
+        <div class="am-container am-margin-vertical-xl">
+            <header class="am-padding-vertical">
+                <h2 class="about-title about-color">菠萝软件|音乐</h2>
+                <p class="am-text-center">全网音乐极速解析下载</p>
+            </header>
+            <div class="am-u-lg-12 am-padding-vertical">
+                <form id="j-validator" class="am-form am-margin-bottom-lg" method="post">
+                    <div class="am-u-md-12 am-u-sm-centered">
+                        <div class="am-form-group">
+                            <input id="j-input" data-filter="name" class="am-form-field am-input-lg am-text-center am-radius" placeholder="例如: 两个自己 G.E.M.邓紫棋" data-am-loading="{loadingText: ' '}" pattern="^.+$" required>
+                            <div class="am-alert am-alert-danger am-animation-shake"></div>
+                        </div>
+                        <div id="j-type" class="am-form-group am-text-center music-type">
+                        <?php foreach ($music_type_list as $key => $val) { ?>
+                            <label class="am-radio-inline">
+                                <input type="radio" name="music_type" value="<?php echo $key; ?>" data-am-ucheck<?php if ($key === 'netease') echo ' checked'; ?>>
+                                <?php echo $val; ?>
+                            </label>
+                            <?php if ($key === 'migu') echo '<br />'; ?>
+                        <?php } ?>
+                        </div>
+                        <button id="j-submit" type="submit" class="am-btn am-btn-primary am-btn-lg am-btn-block am-radius" data-am-loading="{spinner: 'cog', loadingText: '正在GET数据...', resetText: 'Get &#x221A;'}">Get &#x221A;</button>
+                    </div>
+                </form>
+                <form id="j-main" class="am-form am-u-md-12 am-u-sm-centered music-main">
+                    <a type="button" id="j-back" class="am-btn am-btn-success am-btn-lg am-btn-block am-radius am-margin-bottom-lg">成功 Get &#x221A; 返回 <i class="am-icon-reply am-icon-fw"></i></a>
+                    <div class="am-g am-margin-bottom-sm">
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐地址', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-link am-icon-fw"></i></span>
+                                <input id="j-link" class="am-form-field" readonly>
+                                <span class="am-input-group-btn">
+                                    <a id="j-link-btn" class="am-btn am-btn-default" target="_blank">
+                                        <i class="am-icon-external-link"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐链接', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-music am-icon-fw"></i></span>
+                                <input id="j-src" class="am-form-field" readonly>
+                                <span class="am-input-group-btn">
+                                    <a id="j-src-btn" class="am-btn am-btn-default" target="_blank">
+                                        <i id="j-src-btn-icon" class="am-icon-external-link"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="am-g">
+                        
+                        
+                    </div>
+                    <div class="am-g">
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐名称', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-tag am-icon-fw"></i></span>
+                                <input id="j-name" class="am-form-field" readonly>
+                            </div>
+                        </div>
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐作者', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-user am-icon-fw"></i></span>
+                                <input id="j-author" class="am-form-field" readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="j-show" class="am-margin-vertical">
+                        <div id="j-player" class="aplayer"></div>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
+        <div class="am-popup" id="copr-info">
+            <div class="am-popup-inner">
+                
+            </div>
+        </div>
+    </section>
+    
+    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/amazeui/2.3.0/js/amazeui.min.js"></script>
+    <script src="//cdn.bootcss.com/aplayer/1.6.0/APlayer.min.js"></script>
+    <script src="//cdn.bootcss.com/Base64/1.0.1/base64.min.js"></script>
+    <script src="static/js/music.js?v<?php echo MC_VERSION; ?>"></script>
+</body>
+</html>
